5555185609eefb7d840bd5b59c84b881e29559d1
Hkervadec <hoel.kervadec@insa-rennes.fr>
2014-04-08 13:48:22 +0200

    Grammaire Syntax analysis is working, but not the compiler

--- launchTest.py
+++ launchTest.py
@@ -3,14 +3,17 @@
 # Fuck this I don't need to comment it
 
 from subprocess import *
+from sys import exit
 
 testFolder = "test"
 testFiles = ["exemple_entree_sortie.yaka", "expr1.yaka", "expr2.yaka", "expr3.yaka", \
             "expr4.yaka", "expr5.yaka", "err1.yaka", "err2.yaka", "err3.yaka", \
-            "exemple_iteration.yaka", "exemple_conditionnelle.yaka", "iter2.yaka"]
+            "exemple_iteration.yaka", "exemple_conditionnelle.yaka", "iter2.yaka",\
+            "exemple_fct.yaka"]
+
 
 firstCleanup = ["*.class", "TokenMgrError.java", "ParseException.java",\
-                "Token.java", "SimpleCharStream.java"]
+                "Token.java", "SimpleCharStream.java", "Yaka.java"]
 
 shellV = False
 delC = "rm"
@@ -26,7 +29,12 @@ def pPrint(ab):
 
 print("**** Compiling... ****")
 pPrint(check_output([delC, delO] + firstCleanup, shellV, stderr=STDOUT))
-pPrint(check_output("javacc Yaka.jj", shell=True, stderr=STDOUT))
+try:
+    pPrint(check_output("javacc Yaka.jj", shell=True, stderr=STDOUT))
+except CalledProcessError as e:
+    pPrint(e.output)
+    exit("Javacc il est cassé")
+
 pPrint(check_output("javac *.java", shell=True))
 
 print()
